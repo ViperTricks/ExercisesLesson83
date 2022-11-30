5c8ce7cd6e0aa57b8f6011409fc3984467844dca
Hồ Đắc Toàn <hodactoan181905@gmail.com>
2022-11-30 17:45:36 +0700

    finished

---
+++ Ex1/IFilter.cs
@@ -0,0 +1,10 @@
+﻿namespace ExercisesLesson83
+{
+    interface IFilter
+    {
+        bool IsNameValid(string name);
+        bool IsEmailValid(string email);
+        bool IsPhoneValid(string phone);
+        bool IsEmpIdValid(string empId);
+    }
+}
